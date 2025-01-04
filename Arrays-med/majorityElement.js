34a382e7353c0494d495f1bad55c20e75d45a318
Danyal Khan <danyal.khan@ongraph.com>
2025-01-05 01:01:44 +0530

    majorityElements

--- Arrays-med/majorityElement.js
+++ Arrays-med/majorityElement.js
@@ -20,3 +20,34 @@
 // ? Step 2 Verify the number of occurence of the maximum element
 // ? Step 2.1 IntiIterate over the array for(i:0->n)
 // ? Step 2.2 Check if (arr[i] == el) count1++
+// ? Step 2.3 Check if count1 > Math.floor(n/2) return el; where n is size of array
+// ? Step 3 Otherwise return -1, as invalid search
+
+const majorityElement = (nums) => {
+  let count = 0,
+    el = 0;
+  let n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    if (count === 0) {
+      count = 1;
+      el = nums[i];
+    } else if (nums[i] === el) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  let count1 = 0;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === el) count1++;
+  }
+
+  if (count1 > Math.floor(n / 2)) return el;
+  return -1;
+};
+
+const arr = [2, 2, 1, 1, 1, 2, 2];
+
+console.log(majorityElement(arr));
